6426ad82a7946ec9d90e63734f2243df30b994d4
James McClain <jmcclain@azavea.com>
2017-08-03 23:08:31 -0400

    bump resources

--- emr/config-emr.mk
+++ emr/config-emr.mk
@@ -3,5 +3,5 @@ export MASTER_INSTANCE:=m3.xlarge
 export MASTER_PRICE := 0.5
 export WORKER_INSTANCE:=m3.xlarge
 export WORKER_PRICE := 0.5
-export WORKER_COUNT := 7
+export WORKER_COUNT := 32
 export USE_SPOT := true
