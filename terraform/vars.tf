e756b5061baca36ca540049f9625de0033bf43f6
James McClain <jmcclain@azavea.com>
2017-08-28 12:57:23 -0400

    Move `vars` File From JSON to HCL

---
+++ terraform/vars.tf
@@ -0,0 +1,33 @@
+variable "access_key" {
+    type = "string"
+    description = "AWS Access Key (nominally from ~/.aws/credentials)"
+    default = ""
+}
+
+variable "secret_key" {
+    type = "string"
+    description = "AWS Secret Key (nominally from ~/.aws/credentials)"
+    default = ""
+}
+
+variable "region" {
+    type = "string"
+    description = "AWS Region"
+    default = "us-east-1"
+}
+
+variable "pem_path" {
+    type = "string"
+    description = "Path to EC2 secret key"
+}
+
+variable "key_name" {
+    type = "string"
+    description = "The name of your EC2 secret key"
+}
+
+variable "s3_uri" {
+    type = "string"
+    description = "Where to send EMR logs"
+    default = "s3://geotrellis-test/terraform-logs"
+}
