7956ab66f2c016bbdb7c70f3df5ca6b5d811201c
James McClain <jmcclain@azavea.com>
2017-09-01 11:33:20 -0400

    Rename ECS Worker

--- terraform/jupyterhub.tf
+++ terraform/jupyterhub.tf
@@ -1,10 +1,11 @@
-resource "aws_spot_instance_request" "cheap_worker" {
+resource "aws_spot_instance_request" "jupyterhub" {
   ami                  = "${var.ecs_ami}"
   iam_instance_profile = "${var.ecs_instance_profile}"
   instance_type        = "m3.xlarge"
   key_name             = "${var.key_name}"
   security_groups      = ["${aws_security_group.security-group.name}"]
   spot_price           = "0.05"
+  wait_for_fulfillment = true
 
   user_data = "#!/bin/bash\necho ECS_CLUSTER=${aws_ecs_cluster.jupyterhub.name} >> /etc/ecs/ecs.config"
 }
@@ -43,5 +44,5 @@ resource "aws_ecs_service" "jupyterhub" {
 }
 
 output "ecs-instance" {
-  value = "${aws_spot_instance_request.cheap_worker.public_dns}"
+  value = "${aws_spot_instance_request.jupyterhub.public_dns}"
 }
