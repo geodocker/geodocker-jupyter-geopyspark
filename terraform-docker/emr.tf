470c3c8e51cfbede7dae3d86073bd96f36d4f156
James McClain <jmcclain@azavea.com>
2017-10-18 15:59:05 -0400

    Bid Price

--- terraform-docker/emr.tf
+++ terraform-docker/emr.tf
@@ -15,7 +15,7 @@ resource "aws_emr_cluster" "emr-spark-cluster" {
   }
 
   instance_group {
-    bid_price      = "0.05"
+    bid_price      = "${var.bid_price}"
     instance_count = 1
     instance_role  = "MASTER"
     instance_type  = "m3.xlarge"
@@ -23,7 +23,7 @@ resource "aws_emr_cluster" "emr-spark-cluster" {
   }
 
   instance_group {
-    bid_price      = "0.05"
+    bid_price      = "${var.bid_price}"
     instance_count = "${var.worker_count}"
     instance_role  = "CORE"
     instance_type  = "m3.xlarge"
