00a331e9a9a24e83b937292be47289e9396abc4a
James McClain <jmcclain@azavea.com>
2017-12-19 14:06:00 -0500

    Update Versions

--- rpms/build/geopyspark.mk
+++ rpms/build/geopyspark.mk
@@ -1,15 +1,15 @@
 archives/geopyspark-$(GEOPYSPARK_SHA).zip:
 	curl -L "https://github.com/locationtech-labs/geopyspark/archive/$(GEOPYSPARK_SHA).zip" -o $@
 
-archives/geopyspark-netcdf-$(NETCDF_SHA).zip:
-	curl -L "https://github.com/geotrellis/geopyspark-netcdf/archive/$(NETCDF_SHA).zip" -o $@
+archives/geopyspark-netcdf-$(GEOPYSPARK_NETCDF_SHA).zip:
+	curl -L "https://github.com/geotrellis/geopyspark-netcdf/archive/$(GEOPYSPARK_NETCDF_SHA).zip" -o $@
 
 rpmbuild/SOURCES/geopyspark.tar:
 	tar cvf $@ geopyspark/
 
 rpmbuild/RPMS/x86_64/geopyspark-$(GEOPYSPARK_VERSION)-13.x86_64.rpm rpmbuild/RPMS/x86_64/geopyspark-worker-$(GEOPYSPARK_VERSION)-13.x86_64.rpm: rpmbuild/SPECS/geopyspark.spec scripts/geopyspark.sh rpmbuild/SOURCES/geopyspark.tar \
-archives/geopyspark-$(GEOPYSPARK_SHA).zip archives/geopyspark-netcdf-$(NETCDF_SHA).zip archives/$(GEOPYSPARK_JAR) archives/$(NETCDF_JAR)
-	cp -f archives/geopyspark-netcdf-$(NETCDF_SHA).zip archives/geopyspark-netcdf.zip
+archives/geopyspark-$(GEOPYSPARK_SHA).zip archives/geopyspark-netcdf-$(GEOPYSPARK_NETCDF_SHA).zip archives/$(GEOPYSPARK_JAR) archives/$(NETCDF_JAR)
+	cp -f archives/geopyspark-netcdf-$(GEOPYSPARK_NETCDF_SHA).zip archives/geopyspark-netcdf.zip
 	cp -f archives/geopyspark-$(GEOPYSPARK_SHA).zip archives/geopyspark.zip
 	docker run -it --rm \
           -v $(shell pwd)/archives:/archives:ro \
--- rpms/build/rpmbuild/SPECS/geopyspark.spec
+++ rpms/build/rpmbuild/SPECS/geopyspark.spec
@@ -1,7 +1,7 @@
 %define _topdir   /tmp/rpmbuild
 %define name      geopyspark
 %define release   13
-%define version   0.2.2
+%define version   0.3.0
 
 BuildRoot: %{buildroot}
 Summary:   GeoPySpark
@@ -19,7 +19,7 @@ AutoReq:   no
 %global __os_install_post /usr/lib/rpm/brp-compress %{nil}
 
 %description
-GeoPySpark 0.2.2
+GeoPySpark 0.3.0
 
 %prep
 %setup -q -n geopyspark
@@ -35,7 +35,11 @@ tar cvf /tmp/packages.tar $(diff before.txt after.txt | grep '^>' | cut -f2 '-d 
 cd %{buildroot}
 tar axvf /tmp/packages.tar
 mkdir -p %{buildroot}/opt/jars
-cp /archives/gddp-assembly-%{version}.jar /archives/geotrellis-backend-assembly-%{version}.jar %{buildroot}/opt/jars
+cd %{buildroot}/opt/jars
+curl -L -O https://dl.bintray.com/azavea/maven/org/locationtech/geotrellis/geotrellis-backend_2.11/%{version}/geotrellis-backend_2.11-%{version}.jar
+curl -L -O https://dl.bintray.com/azavea/maven/org/locationtech/geotrellis/geopyspark-gddp_2.11/%{version}/geopyspark-gddp_2.11-%{version}.jar
+curl -L -O https://s3.amazonaws.com/geopyspark-dependency-jars/geotrellis-backend-assembly-%{version}-deps.jar
+curl -L -O https://s3.amazonaws.com/geopyspark-dependency-jars/netcdfAll-5.0.0-SNAPSHOT.jar
 
 %package worker
 Summary: GeoPySpark
