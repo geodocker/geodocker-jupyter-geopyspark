dd0488a0a9d2066eb275293f58cfdf20f7d4a54f
James McClain <jmcclain@azavea.com>
2018-01-04 09:27:01 -0500

    Store And Retrive Blobs

--- rpms/build/build.sh
+++ rpms/build/build.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+if [ ! -z "$1" ]
+then
+    URI=$(echo $1 | sed 's,/$,,')
+    make src
+    aws s3 sync $URI rpmbuild/RPMS/x86_64/
+    aws s3 cp $URI/gdal-and-friends.tar.gz blobs/gdal-and-friends.tar.gz
+    touch rpmbuild/RPMS/x86_64/*.rpm
+    touch rpmbuild/RPMS/x86_64/gdal213-*.rpm
+    touch rpmbuild/RPMS/x86_64/jupyterhub-*.rpm
+    touch rpmbuild/RPMS/x86_64/mapnik-*.rpm
+    touch rpmbuild/RPMS/x86_64/python-mapnik-*.rpm
+    touch rpmbuild/RPMS/x86_64/geonotebook-*.rpm
+fi
+
 make gcc4
 make gcc6
 make rpms
--- rpms/build/publish.sh
+++ rpms/build/publish.sh
@@ -2,7 +2,9 @@
 
 if [ ! -z "$1" ]
 then
-    aws s3 sync rpmbuild/RPMS/x86_64/ $(echo $1 | sed 's,/$,,')/$(git rev-parse HEAD)/
+    URI=$(echo $1 | sed 's,/$,,')/$(git rev-parse HEAD)/
+    aws s3 sync rpmbuild/RPMS/x86_64/ $URI
+    aws s3 cp blobs/gdal-and-friends.tar.gz $URI
 else
     echo "Need location"
 fi
