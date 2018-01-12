4d3f6872a8736d282923d38b6eee7c344954ddbc
James McClain <jmcclain@azavea.com>
2018-01-12 10:15:37 -0500

    Build Base Image

--- rpms/build/fetch.sh
+++ rpms/build/fetch.sh
@@ -6,7 +6,10 @@ then
     make src
     aws s3 sync $URI rpmbuild/RPMS/x86_64/
     mv -f rpmbuild/RPMS/x86_64/gdal-and-friends.tar.gz blobs/
+    touch blobs/gdal-and-friends.tar.gz
     touch rpmbuild/RPMS/x86_64/*.rpm
+    touch rpmbuild/RPMS/x86_64/hdf5-*.rpm
+    touch rpmbuild/RPMS/x86_64/netcdf-*.rpm
     touch rpmbuild/RPMS/x86_64/gdal213-*.rpm
     touch rpmbuild/RPMS/x86_64/jupyterhub-*.rpm
     touch rpmbuild/RPMS/x86_64/mapnik-*.rpm
