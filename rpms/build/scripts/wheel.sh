94b1c6b0f5195042cb9a9ff94d5093f5a73ea0e4
James McClain <jmcclain@azavea.com>
2018-02-06 16:27:12 -0500

    Remove Extra/Duplicate Wheels

--- rpms/build/scripts/wheel.sh
+++ rpms/build/scripts/wheel.sh
@@ -24,4 +24,18 @@ pip3.4 install numpy==1.12.1
 pip3.4 wheel -r requirements.txt
 pip3.4 install ipython==5.1.0 /archives/ipykernel.zip
 pip3.4 wheel ipython==5.1.0 /archives/ipykernel.zip
+rm -f decorator-4.2.1-py2.py3-none-any.whl \
+   ipykernel-4.8.0-py3-none-any.whl \
+   ipython_genutils-0.2.0-py2.py3-none-any.whl \
+   jupyter_client-5.2.2-py2.py3-none-any.whl \
+   jupyter_core-4.4.0-py2.py3-none-any.whl \
+   pexpect-4.3.1-py2.py3-none-any.whl \
+   prompt_toolkit-1.0.15-py3-none-any.whl \
+   ptyprocess-0.5.2-py2.py3-none-any.whl \
+   Pygments-2.2.0-py2.py3-none-any.whl \
+   python_dateutil-2.6.1-py2.py3-none-any.whl \
+   pyzmq-16.0.4-cp34-cp34m-manylinux1_x86_64.whl \
+   six-1.11.0-py2.py3-none-any.whl \
+   tornado-4.5.3-cp34-cp34m-linux_x86_64.whl \
+   traitlets-4.3.2-py2.py3-none-any.whl
 chown -R $USERID:$GROUPID .
