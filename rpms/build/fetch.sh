bf09e61e9326b6ec847a33b398a4f660c6b23c70
James McClain <jmcclain@azavea.com>
2018-02-07 11:41:26 -0500

    Update Fetch, Publish Scripts

--- rpms/build/fetch.sh
+++ rpms/build/fetch.sh
@@ -5,8 +5,7 @@ then
     URI=$(echo $1 | sed 's,/$,,')
     make src
     aws s3 sync $URI rpmbuild/RPMS/x86_64/
-    mv -f rpmbuild/RPMS/x86_64/gdal-and-friends.tar.gz blobs/
-    touch blobs/gdal-and-friends.tar.gz
+    mv -f rpmbuild/RPMS/x86_64/*.whl wheel/
     touch rpmbuild/RPMS/x86_64/*.rpm
     touch rpmbuild/RPMS/x86_64/hdf5-*.rpm
     touch rpmbuild/RPMS/x86_64/netcdf-*.rpm
