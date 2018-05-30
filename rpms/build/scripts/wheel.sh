838524ac0ebf75a860c22ed65092bb6619db08be
jpolchlo <jpolchlopek@azavea.com>
2018-05-30 12:43:23 -0400

    Python package version updates
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

--- rpms/build/scripts/wheel.sh
+++ rpms/build/scripts/wheel.sh
@@ -55,5 +55,5 @@ rm -f affine-2.0.0.post1-py3-none-any.whl \
    six-1.11.0-py2.py3-none-any.whl \
    tornado-5.0-cp34-cp34m-linux_x86_64.whl \
    tornado-5.0.2-cp34-cp34m-linux_x86_64.whl \
-   traitlets-4.3.2-py2.py3-none-any.whl
+   traitlets-4.3.1-py2.py3-none-any.whl
 chown -R $USERID:$GROUPID .
